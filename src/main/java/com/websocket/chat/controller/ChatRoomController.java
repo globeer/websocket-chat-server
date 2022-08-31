ea12a3b57c04fda322e6b199a886e41666d24481
globeekim <softeer@naver.com>
2022-08-31 11:02:15 +0900

    websecurity 수정

--- src/main/java/com/websocket/chat/controller/ChatRoomController.java
+++ src/main/java/com/websocket/chat/controller/ChatRoomController.java
@@ -18,45 +18,45 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatRoomController {
 
-    private final ChatRoomRepository chatRoomRepository;
-    private final JwtTokenProvider jwtTokenProvider;
+	private final ChatRoomRepository chatRoomRepository;
+	private final JwtTokenProvider jwtTokenProvider;
 
-    @GetMapping("/room")
-    public String rooms() {
-        return "/chat/room";
-    }
+	@GetMapping("/room")
+	public String rooms() {
+		return "/chat/room";
+	}
 
-    @GetMapping("/rooms")
-    @ResponseBody
-    public List<ChatRoom> room() {
-        List<ChatRoom> chatRooms = chatRoomRepository.findAllRoom();
-        chatRooms.stream().forEach(room -> room.setUserCount(chatRoomRepository.getUserCount(room.getRoomId())));
-        return chatRooms;
-    }
+	@GetMapping("/rooms")
+	@ResponseBody
+	public List<ChatRoom> room() {
+		List<ChatRoom> chatRooms = chatRoomRepository.findAllRoom();
+		chatRooms.stream().forEach(room -> room.setUserCount(chatRoomRepository.getUserCount(room.getRoomId())));
+		return chatRooms;
+	}
 
-    @PostMapping("/room")
-    @ResponseBody
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatRoomRepository.createChatRoom(name);
-    }
+	@PostMapping("/room")
+	@ResponseBody
+	public ChatRoom createRoom(@RequestParam String name) {
+		return chatRoomRepository.createChatRoom(name);
+	}
 
-    @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(Model model, @PathVariable String roomId) {
-        model.addAttribute("roomId", roomId);
-        return "/chat/roomdetail";
-    }
+	@GetMapping("/room/enter/{roomId}")
+	public String roomDetail(Model model, @PathVariable String roomId) {
+		model.addAttribute("roomId", roomId);
+		return "/chat/roomdetail";
+	}
 
-    @GetMapping("/room/{roomId}")
-    @ResponseBody
-    public ChatRoom roomInfo(@PathVariable String roomId) {
-        return chatRoomRepository.findRoomById(roomId);
-    }
+	@GetMapping("/room/{roomId}")
+	@ResponseBody
+	public ChatRoom roomInfo(@PathVariable String roomId) {
+		return chatRoomRepository.findRoomById(roomId);
+	}
 
-    @GetMapping("/user")
-    @ResponseBody
-    public LoginInfo getUserInfo() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String name = auth.getName();
-        return LoginInfo.builder().name(name).token(jwtTokenProvider.generateToken(name)).build();
-    }
+	@GetMapping("/user")
+	@ResponseBody
+	public LoginInfo getUserInfo() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = auth.getName();
+		return LoginInfo.builder().name(name).token(jwtTokenProvider.generateToken(name)).build();
+	}
 }
